2bd35ac472408935ce8d7077d722df12e26a9a22
Marko Uljarevic <marko.uljarevic@procescom.com>
2019-03-29 20:16:13 +0100

    new version -conflict-

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into users(username,password,enabled)
-	values('admin','$2a$10$hbxecwitQQ.dDT4JOFzQAulNySFwEpaFLw38jda6Td.Y/cOiRzDFu',true) if not exists;
+	values('admin','$2a$10$hbxecwitQQ.dDT4JOFzQAulNySFwEpaFLw38jda6Td.Y/cOiRzDFu',true);
 
 insert into authorities(username,authority)
-	values('admin','ROLE_ADMIN') if not exists;
+	values('admin','ROLE_ADMIN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-create table if not exists users(
+create table users(
 	username varchar(50) not null primary key,
 	password varchar(100) not null,
 	enabled boolean not null
 );
-create table if not exists authorities (
+create table authorities (
 	username varchar(50) not null,
 	authority varchar(50) not null,
 	constraint fk_authorities_users foreign key(username) references users(username)
