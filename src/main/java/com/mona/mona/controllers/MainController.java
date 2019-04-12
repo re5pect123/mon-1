cfaf0e0331afb7107bea4503869e4c481cd081f7
Marko Uljarevic <re5pectus123@gmail.com>
2019-04-12 21:56:27 +0200

    dodavanje korisnika GET /kreator-podaci

--- src/main/java/com/mona/mona/controllers/MainController.java
+++ src/main/java/com/mona/mona/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.mona.mona.controllers;
 
 import com.mona.mona.entity.Authorities;
+import com.mona.mona.entity.Kreator;
 import com.mona.mona.repository.AuthoritiesRepository;
+import com.mona.mona.repository.KreatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
@@ -13,6 +16,9 @@ public class MainController {
     @Autowired
     AuthoritiesRepository authoritiesRepository;
 
+    @Autowired
+    KreatorRepository kreatorRepository;
+
     @GetMapping("/login")
     public String korisnik(Principal principal){
         System.out.println("Korisnik " + principal.getName());
@@ -20,5 +26,11 @@ public class MainController {
         System.out.println(a.getUsername());
         System.out.println(a.getAuthority());
         return a.getAuthority();
+    }
+
+    @GetMapping("/kreator-podaci")
+    public Kreator podaci(@RequestParam String username){
+        Kreator k = kreatorRepository.findFirstByUsername(username);
+        return k;
     }
 }
--- src/main/java/com/mona/mona/repository/KreatorRepository.java
+++ src/main/java/com/mona/mona/repository/KreatorRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KreatorRepository extends CrudRepository<Kreator, Long>{
+
+    Kreator findFirstByUsername(String username);
 }
